62440477ade30a80230d5bfee8b296864e79cda0
Rudra Pratap Singh <98338274+rudrapratapsingh2000@users.noreply.github.com>
2025-09-30 21:28:28 +0530

    Delete configserver/activityservice directory

--- configserver/activityservice/src/main/java/com/fitness/activityservice/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitness.activityservice.repository;
-
-import com.fitness.activityservice.model.Activity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ActivityRepository extends MongoRepository<Activity, String> {
-}
